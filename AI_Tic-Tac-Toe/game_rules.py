e7d373808f316bf08ab8f68ec0c2a2adf52d0796
sirohidaksh210-hash <sirohi.daksh.210@gmail.com>
2026-03-31 22:44:36 +0530

    Remove docstring from NexusRules class
    Removed unnecessary docstring from NexusRules class.

--- AI_Tic-Tac-Toe/game_rules.py
+++ AI_Tic-Tac-Toe/game_rules.py
@@ -1,5 +1,5 @@
 class NexusRules:
-    """Handles the mathematical validation of the game state."""
+    
     WIN_COMBOS = [
         (0, 1, 2), (3, 4, 5), (6, 7, 8), # Rows
         (0, 3, 6), (1, 4, 7), (2, 5, 8), # Cols
